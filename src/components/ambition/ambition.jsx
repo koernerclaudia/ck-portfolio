520df9b6696afd6447070796cae0337ca7b1e257
Claudia Koerner <claudia.koerner@gmail.com>
2024-11-02 14:34:34 +0100

    deploy app

--- src/components/ambition/ambition.jsx
+++ src/components/ambition/ambition.jsx
@@ -9,7 +9,7 @@ export const Ambition = () => {
   <div style={{ backgroundColor: "#f3eefa" }}>
     <div className=" container px-4  my-5 text-center" >
         <div className="row flex-lg-row align-items-center g-5 px-4">
-        <div className="col-lg-6 py-3 mb-4">
+        <div className="col-md-6 py-3">
             <h1 className="display-5 fw-bold text-body-emphasis lh-2 mb-4 text-center">My <span className="special-black">Ambition</span></h1>
             <p className="lead">A role focusing on visual design implementations. <br></br>Building tools, websites &amp; apps that help people automate processes and optimize workflows.</p>
             <p className="lead">This could also be working in a venture studio<br></br> setting up the first instance (MVPs) of a<br></br> new product or service using my acquired skills,<br></br> but also nocode and lowcode tools.
@@ -17,7 +17,7 @@ export const Ambition = () => {
 </p>
             
           </div>
-          <div className="col-lg-6 py-3">
+          <div className="col-md-6 py-3">
             <h1 className="display-5 fw-bold text-body-emphasis lh-2 mb-4 text-center"><span className="special-black">Full Stack </span>Trained</h1>
             <p className="lead">During the summer of 2024 I absolved a<br></br>Full Stack Web Development Training with<br></br> CareerFoundry (DEKRA, ...). </p>
             <p className="lead">I did this to build upon my previous<br></br> Web Skills in HTML & CSS (CIW) and turn<br></br>Web Development into a full time career.</p>
