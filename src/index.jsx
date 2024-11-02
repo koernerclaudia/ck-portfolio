7e22449552fb15c2a201aeb6ca0b387d85692ef0
Claudia Koerner <claudia.koerner@gmail.com>
2024-11-02 20:47:05 +0100

    update

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { Main } from "./components/portfolio-main/main";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // import "./index.scss";
-import { HashRouter as Router } from "react-router-dom";
 
 
 
