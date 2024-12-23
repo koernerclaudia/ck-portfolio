ac54a2c18f990f0a4a05b4ac638771f48405204f
Claudia Koerner <claudia.koerner@gmail.com>
2024-12-23 19:45:07 +0100

    update

--- src/components/ambition/ambition.jsx
+++ src/components/ambition/ambition.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import '../../index.scss';
 import Dekra from '../../assets/Dekra-Siegel.png'
-import CIW from '../../assets/CIW.png'
+import CIW from '../../assets/ciw.png'
 
 
 export const Ambition = () => {
